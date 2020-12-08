db1a284e634e95ef776be64ebc876b39d0910d87
Danie666 <38573888+Danie666@users.noreply.github.com>
2020-12-08 07:47:23 -0500

    esto es una prueba 2

--- app/src/main/java/com/rico/porkys/central/firstFragment.java
+++ app/src/main/java/com/rico/porkys/central/firstFragment.java
@@ -25,7 +25,7 @@ public class firstFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+// esto es un  prueba para subir test
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
